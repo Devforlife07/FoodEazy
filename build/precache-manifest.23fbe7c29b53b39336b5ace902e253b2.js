37bc7b5722ab8e2cf144755a3f4f89b54a59d51d
Devforlife07 <Devansh.forlife@gmail.com>
2020-08-28 21:14:25 +0530

    Heroku

---
+++ build/precache-manifest.23fbe7c29b53b39336b5ace902e253b2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "753cef0279d2caec31892ada0e0f4857",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a536cda79eb5a8ffc277",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "ae957d2da06a55aa898e",
+    "url": "/static/css/main.41a31e7c.chunk.css"
+  },
+  {
+    "revision": "a536cda79eb5a8ffc277",
+    "url": "/static/js/2.529d039e.chunk.js"
+  },
+  {
+    "revision": "2a0b20e1d02e77e8b582ff21a215748a",
+    "url": "/static/js/2.529d039e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae957d2da06a55aa898e",
+    "url": "/static/js/main.5ab66fe4.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  },
+  {
+    "revision": "bfb9ef3477e88b1e97f3312130dad23a",
+    "url": "/static/media/404.bfb9ef34.jpg"
+  },
+  {
+    "revision": "96cf34d3ee2addd6e0069957663a8208",
+    "url": "/static/media/Mihir.96cf34d3.jpeg"
+  },
+  {
+    "revision": "41c2a18b6a98ec4013bffd052b75a2b1",
+    "url": "/static/media/check.41c2a18b.png"
+  },
+  {
+    "revision": "4536ae5afa7a38c1dcd89513dab57d85",
+    "url": "/static/media/close.4536ae5a.png"
+  },
+  {
+    "revision": "9f3dc705435876dff82b93a4237b41de",
+    "url": "/static/media/empty.9f3dc705.png"
+  },
+  {
+    "revision": "797bb3689f2760bfdd192ee58b815d7d",
+    "url": "/static/media/log.797bb368.svg"
+  },
+  {
+    "revision": "a8f6abbb4ea76be277e34cbb17ca9175",
+    "url": "/static/media/noItem.a8f6abbb.png"
+  },
+  {
+    "revision": "993f7d292006f5b823f666d3fcb0733b",
+    "url": "/static/media/register.993f7d29.svg"
+  }
+]);
